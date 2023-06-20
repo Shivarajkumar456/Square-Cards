5d5710a04dc07fe71aac9c921eea1a216ea5e217
shivarajkumar <shivaraj1654@gmail.com>
2023-06-20 16:00:59 +0530

    complete code

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0]
+.addEventListener("click", ()=>{
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+});
+
+document.getElementsByClassName("fa-brands fa-youtube")[1]
+.addEventListener("click", ()=>{
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+});
+
+document.getElementsByClassName("fa-brands fa-youtube")[2]
+.addEventListener("click", ()=>{
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+});
+
+document.getElementsByClassName("article")[0]
+.addEventListener("click", ()=>{
+    window.open("https://davidwalsh.name/array-group")
+});
+document.getElementsByClassName("article")[1]
+.addEventListener("click", ()=>{
+    window.open("https://davidwalsh.name/array-group")
+});
+document.getElementsByClassName("article")[2]
+.addEventListener("click", ()=>{
+    window.open("https://davidwalsh.name/array-group")
+});
